33a79cda91837b9573d37391ca65d7cbbf8cbc0e
gona15 <vrmvn0@gmail.com>
2026-07-11 18:26:23 +0300

    Wrap App component with PasswordGate component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import PasswordGate from "./components/PasswordGate.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <PasswordGate>
+      <App />
+    </PasswordGate>
   </React.StrictMode>
 );
